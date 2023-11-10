0ce6e4b10699c5d7af85d17cda519b2bc4f3b984
Ernest Yushkiavichus <kiberturist95@gmail.com>
2023-11-10 17:43:42 +0300

    feat: fawef

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -1,6 +1,6 @@
 /* *******************************************************************************************
  *                                                                                           *
- * Please read the following tutorial before implementing                            *
+ * Please read the following tutorial before imple                           *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates           *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number   *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math     *
